b4fabeecfceaf8aafdc6474fc2c2e1a0930937ce
AlyssonFrancisco <alyssonfrancisco72@gmail.com>
2025-03-13 22:30:00 -0300

    aprendendo Funcoes

---
+++ aprendizado.js
@@ -0,0 +1,17 @@
+const data = [1, 2, 3, 4, 5, 6];
+
+var numeroDaMinhaEscolha = 6;
+
+function procuraNumeroIgual(num) {
+
+for (var x = 0; x < data.length ; x++)
+    
+    if (data[8] == num){
+    return true 
+console.log(procuraNumeroIgual(data)) 
+} {   
+    return false
+}
+    
+
+}
